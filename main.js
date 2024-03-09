49b3327f2b63e89b61cf1bf7c9152a104e37d59d
nfoert <nofoert@gmail.com>
2024-03-08 20:56:59 -0500

    - Add animation when clicking on a link - Add README

--- main.js
+++ main.js
@@ -31,3 +31,28 @@ document.getElementById("links-links-button").addEventListener("click", function
     }, 400);
 
 });
+
+document.addEventListener('mousemove', function(event) {
+    document.getElementById("animation-circle").style.left = event.clientX + "px";
+    document.getElementById("animation-circle").style.top = event.clientY + "px";
+});
+
+for (item in document.querySelectorAll(".link:not(.no-url)")) {
+    try {
+        document.querySelectorAll(".link:not(.no-url)")[item].addEventListener("click", function(event) {
+            event.preventDefault();
+            document.getElementById("animation-circle").style.scale = "2000";
+
+            setTimeout(function() {
+                window.location.href = event.target.closest("a").getAttribute("url");
+            }, 1000);
+
+            setTimeout(function() {
+                document.getElementById("animation-circle").style.scale = "0";
+            }, 3000);
+        })
+    } catch (TypeError) {
+        null;
+    }
+    
+}
