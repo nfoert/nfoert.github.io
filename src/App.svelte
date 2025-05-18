9c20d4c4d14727b95ca41c2c1717ca44429cfa66
nfoert <nofoert@gmail.com>
2025-05-18 12:40:08 -0400

    feat: Add social links

--- src/App.svelte
+++ src/App.svelte
@@ -4,18 +4,20 @@
 
 <main class="bg-slate-700">
     <div class="flex items-center justify-center min-h-screen min-w-screen">
-        <div class="flex flex-col md:flex-row items-center justify-center gap-2">
-            <div class="flex flex-col w-2/3 md:w-1/4 gap-2">
-                <p class="text-2xl font-bold text-white text-right">nfoert</p>
-                <p class="text-md text-white text-right opacity-80">he/him</p>
-                <p class="text-md text-white text-right">Full stack web developer, who cares about open source and has experience in C++ and self hosting</p>
+        <div class="flex flex-col md:flex-row items-center justify-center gap-6">
+            <div class="flex flex-col w-2/3 md:w-1/4 gap-2 items-center md:items-end">
+                <img src="https://github.com/nfoert.png" alt="nfoert" class="w-32 h-32 rounded-full border-2 border-slate-600/50">
+                <p class="text-2xl font-bold text-white text-center md:text-right">nfoert</p>
+                <p class="text-md text-white text-center md:text-right opacity-80">he/him</p>
+                <p class="text-md text-white text-center md:text-right">Full stack web developer, who cares about open source and has experience in C++ and self hosting</p>
             </div>
     
             <div class="bg-slate-700/50 w-full h-2 md:w-2 md:h-auto rounded-full border-slate-600/50 border-2 md:self-stretch"></div>
     
             <div class="flex flex-col w-2/3 md:w-1/4 gap-2">
-                <Link href="https://github.com/nfoert" text="GitHub" icon="ph-bold ph-github-logo" />
-                <Link href="https://github.com/nfoert" text="GitHub" icon="https://raw.githubusercontent.com/FRC-Team3484/open-scouting/refs/heads/main/repo/images/icon.png" />
+                <Link href="https://github.com/nfoert" text="github.com/" bold="nfoert" icon="ph-bold ph-github-logo" />
+                <Link href="https://mastodon.social/@nfoert" text="mastodon.social/" bold="@nfoert" icon="ph-bold ph-mastodon-logo" />
+                <Link href="https://pixelfed.social/@nfoert" text="pixelfed.social/" bold="@nfoert" icon="ph-bold ph-image" />
             </div>
         </div>
     </div>
--- src/lib/Link.svelte
+++ src/lib/Link.svelte
@@ -1,17 +1,19 @@
 <script lang="ts">
-    const { href, text, icon } = $props();
+    const { href, text, bold, icon } = $props();
 </script>
 
 {#if icon.startsWith("https://")}
     <!-- Create link with image icon -->
-    <a {href} class="flex gap-1 items-center text-md px-6 py-2 rounded-full hover:bg-slate-600/40 active:bg-slate-700/60 transition-all hover:scale-105 active:scale-95 text-center md:text-left">
+    <a {href} class="flex items-center md:items-left text-md px-6 py-2 rounded-full hover:bg-slate-600/40 active:bg-slate-700/60 transition-all hover:scale-105 active:scale-95 text-center md:text-left">
         <img src={icon} alt={text} class="w-6 h-6 inline">
-        <span>{text}</span>
+        <span class="ml-1">{text}</span>
+        <span><strong>{bold}</strong></span>
     </a>
 {:else}
     <!-- Create link with Phosphor icon -->
-    <a {href} class="flex gap-1 items-center text-md px-6 py-2 rounded-full hover:bg-slate-600/40 active:bg-slate-700/60 transition-all hover:scale-105 active:scale-95 text-center md:text-left">
+    <a {href} class="flex items-center md:items-left text-md px-6 py-2 rounded-full hover:bg-slate-600/40 active:bg-slate-700/60 transition-all hover:scale-105 active:scale-95 text-center md:text-left">
         <i class={icon}></i>
-        <span>{text}</span>
+        <span class="ml-1">{text}</span>
+        <span><strong>{bold}</strong></span>
     </a>
 {/if}
