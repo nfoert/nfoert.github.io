1a5c965c0ff65dbc39e85c7a1b792ad3d078b855
nfoert <nofoert@gmail.com>
2025-08-18 08:47:25 -0400

    feat: Add project links

--- src/App.svelte
+++ src/App.svelte
@@ -18,15 +18,18 @@
         </a>
         <div class="flex flex-row gap-2 items-center">
             <p class="text-4xl text-white font-mono font-bold">nfoert</p>
-            <p class="text-lg text-white font-mono opacity-70">
-                he/him
-            </p>
+            <p class="text-lg text-white font-mono opacity-70">he/him</p>
         </div>
 
-        <p class="text-2xl text-purple-800 font-mono font-bold">Full Stack Web Developer</p>
+        <p class="text-2xl text-purple-800 font-mono font-bold">
+            Full Stack Web Developer
+        </p>
 
         <p class="text-sm text-white font-mono opacity-70">
-            Hello! I'm nfoert, a software engineer who primarilly focuses on web development. I also experiment with lots of other random stuff, including desktop development and C++. I care a lot about open source, do 3D printing, and use Linux.
+            Hello! I'm nfoert, a software engineer who primarilly focuses on web
+            development. I also experiment with lots of other random stuff,
+            including desktop development and C++. I care a lot about open
+            source, do 3D printing, and use Linux.
         </p>
 
         <p class="text-lg text-white font-mono">
@@ -44,12 +47,118 @@
     <div
         class="flex flex-col gap-4 h-auto md:h-full md:overflow-y-auto min-w-1/2 justify-center items-center md:items-start text-left md:pr-[25%]"
     >
-        <div class="flex flex-col gap-2 items-center md:items-start text-left pl-2">
-            <p class="text-2xl text-white font-mono font-bold"><i class="ph-bold ph-link"></i> Links</p>
-            <Link href="https://github.com/nfoert" text="github.com/" bold="nfoert" icon="ph-bold ph-github-logo" />
-            <Link href="https://mastodon.social/@nfoert" text="mastodon.social/" bold="@nfoert" icon="ph-bold ph-mastodon-logo" />
-            <Link href="https://pixelfed.social/@nfoert" text="pixelfed.social/" bold="@nfoert" icon="ph-bold ph-image" />
-            <Link href="mailto:nfoert@proton.me" text="" bold="nfoert@proton.me" icon="ph-bold ph-envelope" />
+        <div
+            class="flex flex-col gap-2 items-center md:items-start text-left pl-2"
+        >
+            <p class="text-2xl text-white font-mono font-bold">
+                <i class="ph-bold ph-link"></i> Links
+            </p>
+            <Link
+                href="https://github.com/nfoert"
+                text="github.com/"
+                bold="nfoert"
+                icon="ph-bold ph-github-logo"
+            />
+            <Link
+                href="https://mastodon.social/@nfoert"
+                text="mastodon.social/"
+                bold="@nfoert"
+                icon="ph-bold ph-mastodon-logo"
+            />
+            <Link
+                href="https://pixelfed.social/@nfoert"
+                text="pixelfed.social/"
+                bold="@nfoert"
+                icon="ph-bold ph-image"
+            />
+            <Link
+                href="mailto:nfoert@proton.me"
+                text=""
+                bold="nfoert@proton.me"
+                icon="ph-bold ph-envelope"
+            />
+        </div>
+
+        <div
+            class="flex flex-col gap-2 items-center md:items-start text-left pl-2"
+        >
+            <p class="text-2xl text-white font-mono font-bold">
+                <i class="ph-bold ph-squares-four"></i> Projects
+            </p>
+
+            <p class="text-md text-white font-mono ml-6 opacity-70">
+                / Open Source
+            </p>
+            <CollapsableLink
+                text="Open Scouting"
+                icon="https://raw.githubusercontent.com/FRC-Team3484/open-scouting/refs/heads/main/repo/images/icon.png"
+                description="An open source application for easier scouting at FIRST Robotics competitions"
+                buttons={[
+                    {
+                        text: "Source Code",
+                        href: "https://github.com/FRC-Team3484/open-scouting",
+                        icon: "ph-bold ph-github-logo",
+                    },
+                    {
+                        text: "Live Server",
+                        href: "https://open-scouting.duckdns.org",
+                        icon: "ph-bold ph-browser",
+                    },
+                ]}
+                archived={false}
+            />
+            <CollapsableLink
+                text="Cardie"
+                icon="https://raw.githubusercontent.com/nfoert/cardie/refs/heads/main/repo/images/icon_rounded.png"
+                description="An open source business card designer and sharing platform"
+                buttons={[
+                    {
+                        text: "Source Code",
+                        href: "https://github.com/nfoert/cardie",
+                        icon: "ph-bold ph-github-logo",
+                    },
+                ]}
+                archived={true}
+            />
+            <CollapsableLink
+                text="Circles"
+                icon="https://raw.githubusercontent.com/nfoert/circles/refs/heads/main/main/static/main/images/favicon.ico"
+                description="Circles is an open source, decentralized social network that allows users to move and interact within physical spaces."
+                buttons={[
+                    {
+                        text: "Source Code",
+                        href: "https://github.com/nfoert/circles",
+                        icon: "ph-bold ph-github-logo",
+                    },
+                ]}
+                archived={true}
+            />
+            <Link
+                href="https://github.com/nfoert/nfoert.github.io"
+                text="nfoert.github.io"
+                bold=""
+                icon="ph-bold ph-browser"
+            />
+            <Link
+                href="https://github.com/nfoert/niri-dotfiles"
+                text="niri-dotfiles"
+                bold=""
+                icon="ph-bold ph-file"
+            />
+            <Link
+                href="https://github.com/nfoert?tab=repositories"
+                text=""
+                bold="And more..."
+                icon="ph-bold ph-github-logo"
+            />
+
+            <p class="text-md text-white font-mono ml-6 opacity-70">
+                / Client Work
+            </p>
+
+            <Link href="https://dakotascustomcrafts.com" text="Dakota's Custom Crafts" bold="" icon="/dakotascustomcrafts.png" />
+
+            <Link href="mailto:nfoert@proton.me" text="Request a Commission" bold="" icon="ph-bold ph-envelope" />
         </div>
     </div>
 </main>
--- src/lib/SmallLink.svelte
+++ src/lib/SmallLink.svelte
@@ -6,14 +6,14 @@
 
 {#if icon.startsWith("https://")}
     <!-- Create link with image icon -->
-    <a {href} class="flex items-center grow-2 text-sm px-4 py-1 justify-center rounded-full hover:bg-slate-600/40 active:bg-slate-700/60 transition-all hover:scale-105 active:scale-95 text-center text-white">
+    <a {href} class="flex items-center grow-2 text-sm px-4 py-1 justify-center rounded-full hover:bg-slate-600/40 active:bg-slate-700/60 transition-all hover:scale-105 active:scale-95 text-center text-white font-mono">
         <img src={icon} alt={text} class="w-6 h-6 inline">
         <span class="ml-1">{text}</span>
         <span><strong>{bold}</strong></span>
     </a>
 {:else}
     <!-- Create link with Phosphor icon -->
-    <a {href} class="flex items-center grow-2 text-sm px-4 py-1 justify-center rounded-full hover:bg-slate-600/40 active:bg-slate-700/60 transition-all hover:scale-105 active:scale-95 text-center text-white">
+    <a {href} class="flex items-center grow-2 text-sm px-4 py-1 justify-center rounded-full hover:bg-slate-600/40 active:bg-slate-700/60 transition-all hover:scale-105 active:scale-95 text-center text-white font-mono">
         <i class={icon}></i>
         <span class="ml-1">{text}</span>
         <span><strong>{bold}</strong></span>
