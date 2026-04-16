e7d834a883cd0614638eb97b25933b224aec4a57
nfoert <nofoert@gmail.com>
2026-04-15 22:23:20 -0400

    feat: Improve styling of nav bar buttons

--- src/lib/NavBar.svelte
+++ src/lib/NavBar.svelte
@@ -38,10 +38,10 @@
 </script>
 
 <div class="fixed top-0 left-0 text-white font-mono text-2xl flex flex-row gap-2 p-4 bg-slate-900/50 backdrop-blur-md z-5 w-screen flex-wrap">
-    <button onclick={() => scrollToSection("home")} class={location === 'home' ? 'text-green-600' : 'text-white transition-colors'}>nfoert</button>
-    <p>/</p>
-    <button onclick={() => scrollToSection("projects")} class={location === 'projects' ? 'text-green-600' : 'text-white transition-colors'}>projects</button>
-    <p>/</p>
-    <button onclick={() => scrollToSection("skills")} class={location === 'skills' ? 'text-green-600' : 'text-white transition-colors'}>skills</button>
+    <button onclick={() => scrollToSection("home")} class={location === 'home' ? 'text-green-600 hover:text-green-500 transition-colors' : 'text-white/50 hover:text-white transition-colors'}>nfoert</button>
+    <p class="text-white/50">/</p>
+    <button onclick={() => scrollToSection("projects")} class={location === 'projects' ? 'text-green-600 hover:text-green-500 transition-colors' : 'text-white/50 hover:text-white transition-colors'}>projects</button>
+    <p class="text-white/50">/</p>
+    <button onclick={() => scrollToSection("skills")} class={location === 'skills' ? 'text-green-600 hover:text-green-500 transition-colors' : 'text-white/50 hover:text-white transition-colors'}>skills</button>
 </div>
 
