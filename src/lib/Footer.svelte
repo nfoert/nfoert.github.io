dc7c7122628b76211fb8cea04128bbb9c043a0e3
nfoert <nofoert@gmail.com>
2026-06-07 15:30:10 -0400

    style: Improve margin on footer text

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -2,6 +2,6 @@
 
 </script>
 
-<div class="ml-2 mb-2 mt-16">
+<div class="ml-8 mb-8 mt-16">
     <p class="text-white/50 font-mono transition-all text-sm md:text-md">Made by <a href="https://github.com/nfoert" class="underline hover:text-white">nfoert</a> with Svelte and Tailwind CSS</p>
 </div>
